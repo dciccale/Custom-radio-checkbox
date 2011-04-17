2b55236eed9edb102dc298298d950f9ed65d0610
dciccale <dciccale@gmail.com>
2011-04-17 02:40:13 +0200

    added public method, for ajax loaded content

--- js/jquery.custom-radio-checkbox.js
+++ js/jquery.custom-radio-checkbox.js
@@ -8,10 +8,7 @@
 */ 
 (function($) {
 	$.fn.customRadioCheckbox = (function () {
-		var // radio
-			rds = $("input:radio").addClass("hideInput"),
-			// checkbox
-			chs = $("input:checkbox").addClass("hideInput"), 
+		var	context = $("body"),
 			// radio checked class
 			radioCheckedClass = "radio-checked", 
 			// checkbox checked class
@@ -28,54 +25,68 @@
 			opera = $.browser.opera,
 			// other vars
 			fakeInputClone;
-		
-		// only do this if there are radios
-		if(rds.length) {
-			// radio
-			rds.each(function () {			
-				// fake input
-				fakeInputClone = fakeRadio.clone();
 				
-				// if is checked
-				if (this.checked) {
-					// add checked class
-					fakeInputClone.addClass(radioCheckedClass);
-				}
 				
-				// insert fake input
-				this.parentNode.insertBefore( fakeInputClone[0], this );
+		function customRadioCheckbox(context) {
+			context = context || this;
+			
+			var rds = context.find("input:radio").addClass("hideInput"), // radio
+				chs = context.find("input:checkbox").addClass("hideInput"); // checkbox
 				
-				// radio change event
-				$(this).bind("change.styledRadio",function () {
-					// filter by name and remove class from the last radio checked
-					rds.filter("[name=" + this.name + "]").prev().removeClass(radioCheckedClass);
-					$(this).prev().addClass(radioCheckedClass);
+			// only if there are radios
+			if(rds.length) {
+				// radio
+				rds.each(function () {			
+					// fake input
+					fakeInputClone = fakeRadio.clone();
+					
+					// if is checked
+					if (this.checked) {
+						// add checked class
+						fakeInputClone.addClass(radioCheckedClass);
+					}
+					
+					// insert fake input
+					this.parentNode.insertBefore( fakeInputClone[0], this );
+					
+					// radio change event
+					$(this).bind("change.styledRadio",function () {
+						// filter by name and remove class from the last radio checked
+						rds.filter("[name=" + this.name + "]").prev().removeClass(radioCheckedClass);
+						$(this).prev().addClass(radioCheckedClass);
+					});
 				});
-			});
-		}
-		
-		// only do this if there are checkboxes
-		if(chs.length) {
-			// checkbox
-			chs.each(function () {
-				// fake input
-				fakeInputClone = fakeCheckbox.clone();
-				
-				// if is checked
-				if (this.checked) {
-					// add checked class
-					fakeInputClone.addClass(checkboxCheckedClass);
-				}
-				
-				// insert fake input
-				this.parentNode.insertBefore( fakeInputClone[0], this );
+			}
+			
+			// only if there are checkboxes
+			if(chs.length) {
+				// checkbox
+				chs.each(function () {
+					// fake input
+					fakeInputClone = fakeCheckbox.clone();
+					
+					// if is checked
+					if (this.checked) {
+						// add checked class
+						fakeInputClone.addClass(checkboxCheckedClass);
+					}
+					
+					// insert fake input
+					this.parentNode.insertBefore( fakeInputClone[0], this );
 
-				// checkbox change event
-				$(this).bind("change.styledCheckbox",function () {
-					$(this).prev().toggleClass(checkboxCheckedClass);
+					// checkbox change event
+					$(this).bind("change.styledCheckbox",function () {
+						$(this).prev().toggleClass(checkboxCheckedClass);
+					});
 				});
-			});
+			}
+			// make it chainable
+			return context;
 		}
 		
+		customRadioCheckbox(context);
+		
+		return customRadioCheckbox;
+			
 	})();
 })(jQuery);
