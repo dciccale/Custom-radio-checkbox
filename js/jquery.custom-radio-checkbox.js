cd86b0149f09c59318b39b00b88da40d30f69ecf
Denis Ciccale <dciccale@gmail.com>
2012-06-06 13:27:29 +0200

    fix an issue when using .NET as it modifies the name attr introducing $ signs. also improved speed! a lot faster on initialize and when changing radio options

--- js/jquery.custom-radio-checkbox.js
+++ js/jquery.custom-radio-checkbox.js
@@ -1,101 +1,117 @@
-/* jQuery Custom Radio Checkbox Plugin
-* ----------------------------------------------------------
-* Author: Denis Ciccale (dciccale@gmail.com)
-*
-* Dual licensed under the MIT and GPL licenses:
-* http://www.opensource.org/licenses/mit-license.php
-* http://www.gnu.org/licenses/gpl.html
-*/ 
-(function($) {
-	$.fn.customRadioCheckbox = (function (options) {
-		var	// initial context
-			context = $('body'),
-			// checked prefix
-			checkedPrefix = '-checked',
-			// function to force the input change when clicking on the fake input
-			forceChange = function() {
-				// only trigger if the input is not inside a label
-				if (this.parentNode.nodeName.toLowerCase() !== 'label') $(this.previousSibling).trigger('change.crc', [true]); 
-			},
-			// fake input tag
-			fakeInputTag = $(document.createElement('i')).bind('click.crc', forceChange),
-			// object with each fake input and checked class
-			fakeInput = {
-				radio: fakeInputTag.clone(true).addClass('radio'),
-				checkbox: fakeInputTag.clone(true).addClass('checkbox')
-			},
-			// function that inserts the fake input
-			insertFakeInput = function(input, type) {
-				// fake input
-				var fakeInputClone = fakeInput[type].clone(true);
-				
-				// if is already checked add checked class
-				if (input.checked) fakeInputClone.addClass(type + checkedPrefix);
-				
-				// insert the fake input after the input
-				input.parentNode.insertBefore(fakeInputClone[0], input.nextSibling);
-			};
-			
-		// the main function
-		function customRadioCheckbox(_context) {
-			// if context is defined means is the first init, if not use 'this'
-			var context = _context || this;
-			
-			// if context element is not present return nothing, can't chain anyway
-			if(!context.length) return;
-			
-			var rds = context.find('input[type=radio]:not(.hideInput)').addClass('hideInput'), // find & hide radios
-				chs = context.find('input[type=checkbox]:not(.hideInput)').addClass('hideInput'); // find & hide checkboxes
-			
-			// only if there are radios
-			if(rds.length) {
-				rds.type = 'radio';
-				// insert each fake radio
-				$.each(rds, function (i) {
-					insertFakeInput(rds[i], rds.type);
-				});
-				
-				// bind radio change event
-				rds.bind('change.crc', function (e, force) {
-					// uncheck previous and remove checked class
-					if (!force || !this.checked) {
-						// filter by name and remove class from the last radio checked
-						rds.filter('[name=' + this.name + ']').next().removeClass(rds.type + checkedPrefix);
-						// add checked class to this input
-						$(this).next().addClass(rds.type + checkedPrefix);
-					}
-					// if force set to true and is not already checked, check the input
-					if (force && !this.checked) this.checked = true;
-				});
-			}
-			
-			// only if there are checkboxes
-			if(chs.length) {
-				chs.type = 'checkbox';
-				// insert each fake checkbox
-				$.each(chs, function (i) {
-					insertFakeInput(chs[i], chs.type);
-				});
-				
-				// bind checkbox change event
-				chs.bind('change.crc', function (e, force) {
-					// if force set to true, change state
-					if (force) this.checked = !this.checked;
-					
-					// toggle checked class
-					$(this).next().toggleClass(chs.type + checkedPrefix);
-				});
-			}
-			
-			// make it chainable
-			return context;
-		}
-		
-		// first init
-		customRadioCheckbox(context);
-		
-		// return the function for future calls
-		// for example on ajax callback for the loaded content if needed
-		return customRadioCheckbox;
-	})();
-})(jQuery);
+ /*!
+  * jQuery Custom Radio Checkbox Plugin
+  * Copyright (c) 2012 Denis Ciccale (@tdecs)
+  * Released under MIT license (https://raw.github.com/dciccale/Custom-radio-checkbox/master/LICENSE.txt)
+  */
+(function ($) {
+  $.fn.customRadioCheckbox = (function (options) {
+    // initial context
+    var context = $('body'),
+      // checked suffix
+      checkedSuffix = '-checked',
+      // function to force the input change when clicking on the fake input
+      forceChange = function () {
+        // only trigger if the input is not inside a label
+        if (this.parentNode.nodeName.toLowerCase() !== 'label') {
+          $(this.previousSibling).trigger('change.crc', [true]);
+        }
+      },
+      // fake input tag
+      fakeInputTag = $(document.createElement('i')).bind('click.crc', forceChange),
+      // object with each fake input and checked class
+      fakeInput = {
+        radio: fakeInputTag.clone(true).addClass('radio'),
+        checkbox: fakeInputTag.clone(true).addClass('checkbox')
+      },
+      // function that inserts the fake input
+      insertFakeInput = function (inputs) {
+        var input, type = inputs.type, l = inputs.length, fakeInputClone;
+
+        while (l--) {
+          input = inputs[l];
+
+          // fake input
+          fakeInputClone = fakeInput[type].clone(true);
+
+          // if is already checked add checked class
+          if (input.checked) {
+            fakeInputClone.addClass(type + checkedSuffix);
+          }
+
+          // insert the fake input after the input
+          input.parentNode.insertBefore(fakeInputClone[0], input.nextSibling);
+        }
+      };
+
+    // the main function
+    function customRadioCheckbox(_context) {
+      // if context is defined means is the first init, if not use 'this'
+      var context = _context || this, rds, chs, rdsCache = {};
+
+      // if context element is not present return undefined, can't chain anyway
+      if (!context.length) {
+        return;
+      }
+
+      // find & hide radios
+      rds = context.find('input[type=radio]:not(.hideInput)').addClass('hideInput');
+      // find & hide checkboxes
+      chs = context.find('input[type=checkbox]:not(.hideInput)').addClass('hideInput');
+
+      // only apply if there are radios
+      if (rds.length) {
+        rds.type = 'radio';
+        // insert each fake radio
+        insertFakeInput(rds);
+
+        // bind radio change event
+        rds.bind('change.crc', function (e, force) {
+          // uncheck previous and remove checked class
+          if (!force || !this.checked) {
+            // filter by name and remove class from the last radio checked
+            // save this collection in cache obj for faster use
+            if (!rdsCache[this.name]) {
+              rdsCache[this.name] = rds.filter('[name="' + this.name + '"]').next();
+            }
+            rdsCache[this.name].removeClass(rds.type + checkedSuffix);
+
+            // add checked class to this input
+            $(this.nextSibling).addClass(rds.type + checkedSuffix);
+          }
+          // if force set to true and is not already checked, check the input
+          if (force && !this.checked) {
+            this.checked = true;
+          }
+        });
+      }
+
+      // only apply if there are checkboxes
+      if (chs.length) {
+        chs.type = 'checkbox';
+        // insert each fake checkbox
+        insertFakeInput(chs);
+
+        // bind checkbox change event
+        chs.bind('change.crc', function (e, force) {
+          // if force set to true, change state
+          if (force) {
+            this.checked = !this.checked;
+          }
+
+          // toggle checked class
+          $(this.nextSibling).toggleClass(chs.type + checkedSuffix);
+        });
+      }
+
+      // make it chainable
+      return context;
+    }
+
+    // first init
+    customRadioCheckbox(context);
+
+    // return the function for future calls
+    // for example on ajax callback for the loaded content if needed
+    return customRadioCheckbox;
+  }());
+}(jQuery));
